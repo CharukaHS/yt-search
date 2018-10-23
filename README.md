a461357f48ae310d4d2b98aa9b1f94458dff8deb
CharukaHS <Charuka12@gmail.com>
2018-10-23 12:40:47 +0530

    Initial Commit ✨

---
+++ README.md
@@ -0,0 +1,8 @@
+# Search YouTube videos through url
+
+Had to make this because firefox doesn't support search in youtube like in chrome
+
+### How to use
+1. pm2 start index.js --watch
+2. go to [localhost:3000/yt/your search query](http://localhost:3000/yt/your+search+query)
+3. that's it !😊
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/yt/:q', (req, res) => {
+  console.log(new Date(), req.params)
+  const q = req.params.q
+  const str = q.replace(" ", "+") 
+  
+  res.redirect(`https://www.youtube.com/results?search_query=${str}`) 
+})
+
+app.listen(port, () => console.log(`yt-search-redirect running on ${port}!`))
