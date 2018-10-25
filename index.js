ae0a80ee9f482764a2ac61303a8799aee97db76c
CharukaHS <Charuka12@gmail.com>
2018-10-25 21:49:26 +0530

    🛠 Fixed only replace first space to '+'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000
 app.get('/yt/:q', (req, res) => {
   console.log(new Date(), req.params)
   const q = req.params.q
-  const str = q.replace(" ", "+") 
+  const str = q.replace(/ /g, "+") 
   
   res.redirect(`https://www.youtube.com/results?search_query=${str}`) 
 })
